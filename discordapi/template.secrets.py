9aab9bd0ebbe6217aa1f166cb6b7b8527760c1cc
Dylan Wyatt <141169543+telosdwy@users.noreply.github.com>
2023-08-01 12:21:38 -0500

    Create template.secrets.py

---
+++ discordapi/template.secrets.py
@@ -0,0 +1,3 @@
+CLIENT_ID = 0000000000000000000
+CLIENT_SECRET = 'A client secret obtained from https://discord.com/developers/applications/CLIENT_ID/oauth2/general'
+BOT_TOKEN = 'The bot secret obtained from https://discord.com/developers/applications/CLIENT_ID/bot'
